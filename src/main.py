7b55d476cdc46770dc0f4aa0a77caab0a01711f0
Hải Đào <haigr2004@gmai.com>
2024-10-31 16:50:34 +0700

    main

--- src/main.py
+++ src/main.py
@@ -1,13 +1,1 @@
-from search_algorithms import _utils
-from search_algorithms import BFS
 
-
-def main():
-    listMap = _utils.listMaps()
-    print(listMap)
-    map_number = input("Enter the map number you want to use: ")
-    map_filename = "Map" + map_number + ".txt"
-    BFS.execute(map_filename)
-
-if __name__ == '__main__':
-    main()
